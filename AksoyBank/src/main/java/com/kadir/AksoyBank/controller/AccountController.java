4a647af1b312abf7936e13d39cfb1c1387907771
Kadir Baran Aksoy <kadirbaranaksoy@icloud.com>
2023-02-21 18:13:49 +0300

    Update AccountController.java

--- AksoyBank/src/main/java/com/kadir/AksoyBank/controller/AccountController.java
+++ AksoyBank/src/main/java/com/kadir/AksoyBank/controller/AccountController.java
@@ -1,4 +1,57 @@
 package com.kadir.AksoyBank.controller;
 
+
+import com.kadir.AksoyBank.dto.AccountDto;
+import com.kadir.AksoyBank.service.AccountService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/accounts")
 public class AccountController {
+
+    private final AccountService accountService;
+
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @PostMapping
+    public ResponseEntity<AccountDto> createAccount(AccountDto accountDto){
+        return ResponseEntity.ok(accountService.createAccount(accountDto));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AccountDto>> getAllAccounts() {
+        return ResponseEntity.ok(accountService.getAllAccount());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AccountDto> getAccountById(@PathVariable String id) {
+        return ResponseEntity.ok(accountService.getAccountById(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<AccountDto> updateAccount(@PathVariable String id, @RequestBody AccountDto accountDto) {
+        return ResponseEntity.ok(accountService.updateAccount(id,accountDto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteAccount(@PathVariable String id) {
+        accountService.deleteAccount(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/withdraw/{id}/{amount}")
+    public ResponseEntity<AccountDto> withdrawMoney(@PathVariable String id, @PathVariable Double amount) {
+        return ResponseEntity.ok(accountService.withdrawMoney(id, amount));
+    }
+
+    @PutMapping("/add/{id}/{amount}")
+    public ResponseEntity<AccountDto> addMoney(@PathVariable String id, @PathVariable Double amount) {
+        return ResponseEntity.ok(accountService.addMoney(id, amount));
+    }
+
 }
